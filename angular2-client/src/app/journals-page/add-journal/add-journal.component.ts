32db64bf2ed8efd05e7af41b492223e6dd30b3bc
braune13 <braune@rpi.edu>
2017-03-27 17:14:42 -0400

    journals in local storage are updated when user adds a new journal

--- angular2-client/src/app/journals-page/add-journal/add-journal.component.ts
+++ angular2-client/src/app/journals-page/add-journal/add-journal.component.ts
@@ -8,23 +8,41 @@ import { JournalsService } from '../../_services/index';
   providers: [JournalsService]
 })
 export class AddJournalComponent {
+  public user = JSON.parse(localStorage.getItem('currentUser'));
+  public userid = this.user._id;
 
   constructor(
     private journalsService: JournalsService,
   ) { }
 
-  addJournal(newTitle: string) {
-    let currentUser = JSON.parse(localStorage.getItem('currentUser'));
-    var userid = currentUser._id;
+  //------------------------------------------------------------------------------------------------------------------------------
+  //Adds journal to database, tied to user's id
 
-    this.journalsService.create(userid, newTitle)
+  addJournal(newTitle: string) {
+    this.journalsService.create(this.userid, newTitle)
       .subscribe(
         data => {
           console.log(data.message);
           console.log("ID of new journal:  " + data.id);
+          //update journals in local storage
+          this.getJournals();
         },
         error => {
           console.log("Adding journal failed:  " + error._body);
+        });
+  }
+  //------------------------------------------------------------------------------------------------------------------------------
+  //Gets all journals tied to a userid
+
+  getJournals() {
+    this.journalsService.getAllJournals(this.userid)
+      .subscribe(
+        data => {
+          //output updated local storage
+          console.log(JSON.parse(localStorage.getItem('userJournals')));
+        },
+        error => {
+          console.log("Getting journals failed:  " + error._body);
         });
   }
 }
