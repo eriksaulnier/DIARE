ef6221bf0d7a3fecc08efcfccdfc1403d72b621a
braune13 <braune@rpi.edu>
2017-04-26 09:20:39 -0400

    fixing navigation bar bug

--- angular2-client/src/app/app.component.ts
+++ angular2-client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'DIARE';
   isIn = false;
+  //journalsActive = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   toggleState() {
       let bool = this.isIn;
@@ -20,5 +27,50 @@ export class AppComponent {
 		else
 			return false;
 	}
-
+  //------------------------------------------------------------------------------------------------------------------------------
+  journalsActive() {
+    if (this.router.url === "/journals") {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  //------------------------------------------------------------------------------------------------------------------------------
+  aboutActive() {
+    if (this.router.url === "/about") {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  //------------------------------------------------------------------------------------------------------------------------------
+  settingsActive() {
+    if (this.router.url === "/settings") {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  //------------------------------------------------------------------------------------------------------------------------------
+  homeActive() {
+    if (this.router.url === "/home") {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  //------------------------------------------------------------------------------------------------------------------------------
+  registerActive() {
+    if (this.router.url === "/register") {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  //------------------------------------------------------------------------------------------------------------------------------
 }
